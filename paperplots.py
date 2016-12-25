5dfc2fc9060ea6fac499f0b4c8bddd9b82243d49
ssroffe <ssroffe@gmail.com>
2016-12-25 18:53:11 -0500

    velocity curves

--- paperplots.py
+++ paperplots.py
@@ -1,12 +1,40 @@
+### STUFF FOR PAPER
+
+"""Sine model for fitting"""
 def sine(t,A,P,Phi,Gamma):
     import numpy as np
     return A*np.sin((2*np.pi)*(t/P) + Phi) + Gamma
 
+"""FOR VELOCITY FITTING"""
+def voigtModel(x, Ldepth, Lwidth, Gdepth, Gwidth):
+    return 1.0-Ldepth/(1.0 + ((x)/Lwidth)**2) - Gdepth*np.exp(-(x)**2/(2*Gwidth**2))
+
+def lnlikeModel(p,x,y,err):
+    Ldepth,Lwidth,Gdepth,Gwidth = p
+    return -np.sum((y-voigtModel(x,Ldepth,Lwidth,Gdepth,Gwidth))**2/(2*err))
+
+def lnpriorModel(p):
+    Ldepth,Lwidth,Gdepth,Gwidth = p
+    if 0.0 < Ldepth < 1.0 and 0.0 < Lwidth < 3000.0 and 0.0 < Gdepth < 1.0 and 0.0 < Gwidth < 600.0:
+        return 0.0
+    return -np.inf
+
+def lnprobModel(p, x, y,yerr):
+    lp = lnpriorModel(p)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + lnlikeModel(p, x, y, yerr)
+
+def voigt(x, Ldepth, Lwidth, Gdepth, Gwidth, RVShift):
+    return 1.0-Ldepth/(1.0 + ((x-RVShift)/Lwidth)**2) - Gdepth*np.exp(-(x-RVShift)**2/(2*Gwidth**2))
+
+"""Split the large gaps in the x-axis for the time plot"""
 def splitAxis(num):
     import matplotlib.pyplot as plt
     axes = plt.subplots(1,num,sharey=True,facecolor='w',figsize=(25,10))
     return  axes
 
+"""Make the plots look pretty"""
 def setFig():
     import matplotlib.pyplot as plt
     plt.rcdefaults()
@@ -26,6 +54,7 @@ def setFig():
     plt.rcParams.update({'ytick.minor.width': 1.25 })
     #plt.figure(1,figsize = [11.0, 8.5])
 
+"""Plot orbital solution vs time"""
 def TimePlot():
     import tools as tls
     import numpy as np
@@ -115,6 +144,7 @@ def TimePlot():
     
     #plt.show()
 
+"""Plot phase-folded orbit"""
 def PhasePlot():
     import tools as tls
     import numpy as np
@@ -200,6 +230,8 @@ def PhasePlot():
     plt.savefig("../../PaperPlots/"+wdName+"/"+wdName+"_phase.pdf")
     #plt.show()
 
+
+"""Calculate Signal to Noise for spectra"""
 def Signal2Noise():
     from numpy import array, where, median, abs
     import tools as tls
@@ -220,6 +252,8 @@ def Signal2Noise():
             s2n.append(float(signal/noise))
     return array(s2n)
 
+
+"""make the table of rv data for Latex"""
 def LatexTable():
     import numpy as np
     import tools as tls
@@ -256,16 +290,114 @@ def LatexTable():
             nameCol.append("...")
             
     ascii.write([nameCol,timeArr,rvCol],format="latex")
-    
-    
 
+"""Velocity plots"""
+
+"""get the model Parameters to plot"""
+def GetModelVelocity(Hline="gamma"):
+    import tools as tls
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from astropy.io import fits
+    
+    
+    Objects = {"wd1235" : "J123549.89+154319.3", "wd1203" : "J120315.22+650524.4",
+               "wd1140" : "J114024.02+661842.2", "wd1121" : "J112105.23+644336.4",
+               "wd0907" : "J090751.78+071844.6", "wd0343" : "J034319.09+101238.0"}
+
+    HLines = { "beta" : 0, "gamma" : 1, "delta" : 2 }
+    Hl = HLines[Hline]
+    
+    lines = [line.rstrip('\n') for line in open('filelist')]
+    path = lines[0]
+    basename = os.path.basename(path)[:-5]
+    wdName = basename[0:6]
+    modelFile = "../../KoesterModels/da"+open('modelVal').read().splitlines()[0]+".dk"
+    modelWl,modelFlux = tls.ModelNormNoPlot(modelFile)
+    tmpWl,tmpFlux,_ = tls.NormNoPlot(lines[0])
+    modelVels,modelFluxes = tls.ModelGetAllVelocities(modelFile)
+    modelErrs = []
+    for i in range(len(modelFluxes)):
+        modelErrs.append(0.01*modelFluxes[i])
+
+    mdim,mwalkers = 4,200
+    vel = modelVels[Hl]
+    flux = modelFluxes[Hl]
+    err = modelErrs[Hl]
+    modelSampler = tls.MCMCfit(lnprobModel,args=(np.array(vel),np.array(flux),np.array(err)),nwalkers=mwalkers,ndim=mdim,burnInSteps=16000,steps=16000)
+    modelSamples = modelSampler.flatchain.reshape((-1,mdim)).T
+
+    ld = modelSamples[0].mean()
+    lw = modelSamples[1].mean()
+    gd = modelSamples[2].mean()
+    gw = modelSamples[3].mean()
+
+    modelParams = np.array([ld,lw,gd,gw])
+    np.savetxt("BICFits/"+wdName+"_"+Hline+"_modelParams.csv",modelParams,delimiter=',')
+
+"""Actually plot the velocity curves"""
+def PlotVelocities(Hline="gamma"):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    HLines = { "beta" : 0, "gamma" : 1, "delta" : 2 }
+    Hl = HLines[Hline]
+    lines = [line.rstrip('\n') for line in open('filelist')]
+    tmppath = lines[0]
+    basename = os.path.basename(tmppath)[:-5]
+    wdName = basename[0:6]
+
+    
+    ld,lw,gd,gw = np.genfromtxt("BICFits/"+wdName+"_"+Hline+"_modelParams.csv",delimeter=',')
+    rvdata = np.genfromtxt("BICFits/"+wdName+"_rvdata.csv",delimiter=',')
+    timeArr,rvArr,stdArr = rvdata
+
+    off = 0.5
+
+    if len(lines) >= 10:
+        halfway = int(len(lines)/2)
+    else:
+        halfway = 999
+        
+    setfig()    
+    for j in range(len(lines)):
+        path = lines[j]
+        c = 299792.458
+        vels,fluxes,ferrs = tls.GetAllVelocities(path)
+        vels = vels[Hl]
+        fluxes = fluxes[Hl]
+        ferrs = ferrs[Hl]
+        f, (ax1,ax2) = plt.subplots(1,2,sharey=True)
+        if j <= halfway:
+            k = j
+            ax1.step(vels,fluxes+k*off,where='mid',linewidth=1.5,color='k')
+            ax1.plot(vels,voigt(vels,ld,lw,gd,gw,rvArr[j])+j*off,color='r',linewidth=1.5,label='Best fit')
+            ax1.plot(vels,voigt(vels,ld,lw,gd,gw,rvArr[j]+stdArr[j])+k*off,color='cyan',linewidth=1.5,label='Best fit $\pm$ std')
+            ax1.plot(vels,voigt(vels,ld,lw,gd,gw,rvArr[j]-stdArr[j])+k*off,color='cyan',linewidth=1.5)
+            plt.ylabel("Normalized Flux + offset")
+            plt.xlabel("Velocity [km s$^{-1}$]")
+        else:
+            k = j - halfway
+            ax2.step(vels,fluxes+k*off,where='mid',linewidth=1.5,color='k')
+            ax2.plot(vels,voigt(vels,ld,lw,gd,gw,rvArr[j])+j*off,color='r',linewidth=1.5,label='Best fit')
+            ax2.plot(vels,voigt(vels,ld,lw,gd,gw,rvArr[j]+stdArr[j])+k*off,color='cyan',linewidth=1.5,label='Best fit $\pm$ std')
+            ax2.plot(vels,voigt(vels,ld,lw,gd,gw,rvArr[j]-stdArr[j])+k*off,color='cyan',linewidth=1.5)
+            plt.xlabel("Velocity [km s$^{-1}$]")
+            
+    
 def PlotAll():
     TimePlot()
     PhasePlot()
+
+def makeVelocityCurves():
+    GetModelVelocity()
+    PlotVelocities()
     
 if __name__ == '__main__':
     #TimePlot()
     #PhasePlot()
-    PlotAll()
+    #PlotAll()
     #Signal2Noise()
     #LatexTable()
+
+    GetModelVelocity()
